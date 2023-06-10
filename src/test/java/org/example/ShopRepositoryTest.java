5d2cfdfb11edfdec39d43076bbc8a885424bfa5e
Александр Бондаренко <nice.alexsandr@mail.ru>
2023-06-10 17:25:13 +0300

    add test

--- src/test/java/org/example/ShopRepositoryTest.java
+++ src/test/java/org/example/ShopRepositoryTest.java
@@ -9,6 +9,7 @@ class ShopRepositoryTest {
 
     Product item = new Product(1, "элем I", 111);
     Product item2 = new Product(2, "элем II", 222);
+    Product item3 = new Product(3, "элем III", 333);
 
     @Test
     public void successDeletingExistingElement() {
@@ -20,6 +21,20 @@ class ShopRepositoryTest {
         shopRepository.remove(item2.getId());
 
         Product[] expected = {item};
+        Product[] actual = shopRepository.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void successAddElement() {
+        ShopRepository shopRepository = new ShopRepository();
+
+        shopRepository.add(item);
+        shopRepository.add(item2);
+        shopRepository.add(item3);
+
+        Product[] expected = {item,item2,item3};
         Product[] actual = shopRepository.findAll();
 
         Assertions.assertArrayEquals(expected, actual);
